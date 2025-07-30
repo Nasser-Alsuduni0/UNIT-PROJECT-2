b5272e0beff851cbb6f3bb432ad99c3879db448c
Nasser-Alsuduni0 <Nasser.alsuduni@outlook.sa>
2025-07-30 20:30:44 +0300

    ready to deploy

--- ReadWise/ReadWise/settings.py
+++ ReadWise/ReadWise/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-&s-r-tdq+428(p*=^16$n!bv)q^%gvn*&sjvqe6alpfpd-kkn_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,10 +84,21 @@ WSGI_APPLICATION = 'ReadWise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ('PGDATABASE'),
+            'USER': os.environ('PGUSER'),
+            'PASSWORD': os.environ('PGPASSWORD'),
+            'HOST': os.environ('PGHOST'),
+            'PORT': os.environ('PGPORT'),
+        }
+}if not DEBUG else {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+       }
 }
+
+
 
 
 # Password validation
@@ -124,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
